d01c95ac92f71cdbe0881c5772853d9901040148
Braeden Wings <braedenmwings@gmail.com>
2025-10-16 02:33:39 -0400

    Lab 8 files

---
+++ Programming Problem Solving/Lab8A.py
@@ -0,0 +1,32 @@
+def mailing_list():
+    emails = []
+    while True:
+        print("1 - Add email\n2 - Delete email\n3 - List all emails\n4 - Quit\n")
+        choice = int(input("Make your selection: "))
+        if choice == 1:
+            email = input("Enter the email to be added: ")
+            emails.append(email)
+            print("Email added to mailing list")
+        elif choice == 2:
+            deleteEmail = input("Enter Email to be deleted: ")
+            for i in emails:
+                if deleteEmail in emails:
+                    emails.remove(deleteEmail)
+                    print("Email deleted from mailing list")
+                else:
+                    print("Email not found in mailing list")
+        elif choice == 3:
+            if emails:
+                print("Emails in mailing list: ")
+                for email in emails:
+                    print(email)
+                else:
+                    print("Mailing list is empty")
+        elif choice == 4:
+            print("Shutting down...")
+            break
+        else:
+            print("Invalid choice. Please select 1-4.")
+
+if __name__ == "__main__":
+    mailing_list()
---
+++ Programming Problem Solving/Lab8B.py
@@ -0,0 +1,21 @@
+def friendList():
+    # a list which will contain tuples
+    friends = []
+    while True:
+        print("1 - Add friend\n2 - List friends\n3 - Quit")
+        choice = int(input("Make your selection: "))
+        if choice == 1:
+            friendName = input("Enter your friend's name: ")
+            friendAge = int(input("Enter your friend's age: "))
+            friends.append((friendName, friendAge))
+        elif choice == 2:
+            for friend in friends:
+                print(f"Name: {friend[0]}, Age: {friend[1]}")
+        elif choice == 3:
+            print("Shutting down...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    friendList()
